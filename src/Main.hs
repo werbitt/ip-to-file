503e41ae53620602d4b48cea390d0f12d5b0a6d0
Micah Werbitt <micah@werbitt.net>
2014-10-11 14:24:19 -0400

    Command line options
    -f filepath
    -u url for website to get IP
    -U file for list of urls

--- src/Main.hs
+++ src/Main.hs
@@ -17,13 +17,12 @@ import Options.Applicative (
   , auto
   )
 
-import Prelude hiding (FilePath)
---import System.Path (AbsFile)
-
-data Options = Options { path :: String  } deriving (Read, Show)
+data Options = Options { path :: String
+                       , url :: String
+                       , urlFile :: String} deriving (Show)
 
 parseOptions :: Parser Options
-parseOptions = Options <$> parsePath
+parseOptions = Options <$> parsePath <*> parseUrl <*> parseUrlFile
 
 parsePath :: Parser String
 parsePath = strOption $
@@ -31,11 +30,23 @@ parsePath = strOption $
             long "filepath" <>
             help "Filepath to save your IP to"
 
+parseUrl :: Parser String
+parseUrl = strOption $
+           short 'u' <>
+           long "url" <>
+           help "Web url to get your IP from"
 
+parseUrlFile :: Parser String
+parseUrlFile = strOption $
+               short 'U' <>
+               long "url-file" <>
+               help "File with a list of urls to use to get your IP"
 
 withInfo :: Parser a -> String -> ParserInfo a
 withInfo opts desc = info (helper <*> opts) $ progDesc desc
 
+getOptions :: IO Options
+getOptions = execParser $ withInfo parseOptions "Write your IP to a file"
 run :: Options -> IO ()
 run opts = putStrLn $ path opts
 
