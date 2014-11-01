2bcd7037dcb8e2c6093941263284f25454665aff
Micah Werbitt <micah@werbitt.net>
2014-11-01 13:56:08 -0400

    async requests

--- src/Iptf/Ip.hs
+++ src/Iptf/Ip.hs
@@ -2,18 +2,30 @@
 
 module Iptf.Ip where
 
+import           Control.Concurrent.Async
+import           Control.Monad.IO.Class
 import qualified Data.ByteString.Lazy.Char8 as L
 import           GHC.Exception
 import           Network.HTTP.Conduit
 
-
+testUrls :: [String]
+testUrls = [ "http://ipecho.net/plain"
+       , "http://icanhazip.com"
+       ]
 
 ip :: IO L.ByteString
 ip = simpleHttp "http://ipecho.net/plain"
 
-noisyParseUrl :: String -> IO (Maybe Request)
-noisyParseUrl = noisyUrl . parseUrl
+mkRequests :: [String] -> [Either SomeException Request]
+mkRequests urls = map parseUrl urls
 
-noisyUrl :: Either SomeException Request -> IO (Maybe Request)
-noisyUrl (Left e) = putStrLn (show e) >> return Nothing
-noisyUrl (Right r) = return (Just r)
+asyncIp :: [Request] -> IO L.ByteString
+asyncIp rs  = asyncRequests >>= waitAnyCancel >>= return . snd
+   where
+     asyncRequests = sequence $ map (async . get) rs
+     get req =  liftIO $ fmap responseBody $ withManager (httpLbs req)
+
+goodRequests :: [Either SomeException Request] -> [Request]
+goodRequests = foldr go []
+  where go (Left _) xs = xs
+        go (Right x) xs = x:xs
