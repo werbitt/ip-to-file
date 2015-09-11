917feee64c5adf284ca81ca4f7b93c692d60910c
Micah Werbitt <micah@werbitt.net>
2015-09-11 19:45:03 -0400

    Move the main run function to app

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,12 @@
 module Main where
 
-import Iptf
+import           Iptf         (ipFromWeb, updateHostsFile)
 import           Iptf.Options (Options (..), getOptions)
 
 main :: IO ()
-main = run
+main = do
+  opts <- getOptions
+  ip <- ipFromWeb $ url opts
+  case ip of
+    Left e -> putStrLn e
+    Right ip' -> updateHostsFile ip' (hostname opts) (path opts)
--- src/Iptf.hs
+++ src/Iptf.hs
@@ -1,20 +1,23 @@
-module Iptf where
+module Iptf
+       (
+         updateHostsFile
+       , ipFromWeb
+       ) where
 
-import           Control.Applicative
-import           Data.Text.IO (writeFile)
-import           Iptf.Hosts
-import           Iptf.Ip
-import           Prelude hiding (writeFile)
+import           Iptf.Hosts (updateHostsFile)
+import           Iptf.Ip    (IP, ipFromWeb)
 
 
-run :: IO ()
-run = do
-  opts <- getOptions
-  tryWebIP <- ipFromWeb (url opts)
-  case tryWebIP of
-   Right webIP -> do
-     hosts <- getHosts (path opts)
-     if pure webIP == ipForHostname (hostname opts) hosts
-     then return ()
-     else writeFile (path opts) $ toText (update hosts (hostname opts) webIP)
-   Left e -> putStrLn e
+
+
+-- run :: IO ()
+-- run = do
+--   opts <- getOptions
+--   tryWebIP <- ipFromWeb (url opts)
+--   case tryWebIP of
+--    Right webIP -> do
+--      hosts <- getHosts (path opts)
+--      if pure webIP == ipForHostname (hostname opts) hosts
+--      then return ()
+--      else writeFile (path opts) $ toText (update hosts (hostname opts) webIP)
+--    Left e -> putStrLn e
