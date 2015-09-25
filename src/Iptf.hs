fe0d3d4e1687a2757fd789013b573db4c050074f
Micah Werbitt <micah@werbitt.net>
2015-09-25 08:14:27 -0400

    Remove commented out run function

--- src/Iptf.hs
+++ src/Iptf.hs
@@ -6,18 +6,3 @@ module Iptf
 
 import           Iptf.Hosts (updateHostsFile)
 import           Iptf.Ip    (IP, ipFromWeb)
-
-
-
-
--- run :: IO ()
--- run = do
---   opts <- getOptions
---   tryWebIP <- ipFromWeb (url opts)
---   case tryWebIP of
---    Right webIP -> do
---      hosts <- getHosts (path opts)
---      if pure webIP == ipForHostname (hostname opts) hosts
---      then return ()
---      else writeFile (path opts) $ toText (update hosts (hostname opts) webIP)
---    Left e -> putStrLn e
