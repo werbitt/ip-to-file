ec01f73ffd5107a888bcccb7201876b79ba55213
Micah Werbitt <micah@werbitt.net>
2015-09-25 08:11:01 -0400

    Add arbitrary instances

--- test/Iptf/Hosts_Test.hs
+++ test/Iptf/Hosts_Test.hs
@@ -1,6 +1,12 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Iptf.Hosts_Test where
+
+import           Control.Applicative   ((<$>), (<*>))
+import           Data.Monoid           ((<>))
+import           Data.Text             (Text, pack)
 import           Iptf.Hosts.Internal
 import           Iptf.Ip.Internal
 import           Test.Tasty
@@ -15,3 +21,25 @@ getHostsTest = TestCase $ assertEqual
                "One IP and Host"
                "1.2.3.4\tfoo\n"
                (hostsToText $ fromList [Record (IP 1 2 3 4) [Hostname "foo"]])
+instance Arbitrary Hostname where
+  arbitrary = do
+    name <- listOf1 (choose ('a', 'z'))
+    return $ (mkHostname . pack) (name <> ".global")
+
+instance Arbitrary IP where
+  arbitrary = do
+    [a, b, c, d] <- vector 4
+    return $  IP a b c d
+
+instance Arbitrary Record where
+  arbitrary = Record <$> arbitrary <*> listOf arbitrary
+
+instance Arbitrary Hosts where
+  arbitrary = fromList <$> listOf arbitrary
+
+instance Arbitrary Text where
+  arbitrary = pack <$> arbitrary
+
+instance Arbitrary HostsFileContents where
+  arbitrary = HostsFileContents <$> arbitrary <*> arbitrary <*> arbitrary
+
