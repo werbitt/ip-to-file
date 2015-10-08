3b2450a85babb82ea5bf90d9aa7c17cdc4fa1be7
Micah Werbitt <micah@werbitt.net>
2015-10-08 16:56:27 -0400

    Change updfateHfc to updateHostsFile
    I thought I fixed this already, but apparantly there was a straggler

--- src/Iptf/Hosts.hs
+++ src/Iptf/Hosts.hs
@@ -5,8 +5,7 @@ module Iptf.Hosts
        , hostname
        ) where
 
-import           Iptf.Hosts.Internal (Hostname, Modifiable (..), hostname,
-                                      updateHfc)
+import           Iptf.Hosts.Internal (Hostname, hostname, updateHostsFile)
 import           Iptf.Hosts.IO       (readHosts, writeHosts)
 import           Iptf.Ip.Internal    (IP)
 
@@ -14,7 +13,7 @@ updateHosts :: IP -> Hostname -> FilePath -> IO ()
 updateHosts ip name file = do
   hosts <- readHosts file
   case hosts of
-    Right h -> case updateHfc h ip name of
+    Right h -> case updateHostsFile h ip name of
       Changed h' ->  writeHosts file h'
       Same _ -> return ()
     Left  e -> error e
